3f05f55464e8da15066bdb46a95137952beef769
Philipp <psypdt@nottingham.ac.uk>
2019-06-16 15:53:46 +0200

    Update day 1-2 slides, add day 3 exercises

---
+++ Day_3/Day_3_Exercises/99_Bottles.cpp
@@ -0,0 +1,30 @@
+#include <cstdlib>
+#include <iostream>
+
+
+// Create a function that will follow the following pattern:
+/*
+99 bottles of stuff on the wall, 99 bottles of stuff.
+Take one down pass it around 98 bottle of stuff on the wall.
+
+98 bottles of stuff on the wall, 99 bottles of stuff.
+Take one down pass it around 97 bottle of stuff on the wall.
+...
+...
+...
+1 bottle of stuff on the wall, 1 bottle of stuff.
+Take it down pass it around 0 bottles of stuff on the wall.
+0 bottles of stuff on the wall 0 bottles of stuff. 
+Can't take it down, can't pass it around because there are 0 bottles of stuff on the wall.
+ */
+
+
+// Your function should work with all number that are strictly greater than 0
+// Create your function here
+
+
+
+int main()
+{
+    //insert code here
+}
---
+++ Day_3/Day_3_Exercises/Pyramide_drawing.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+using namespace std;
+
+
+
+
+// Construct a function that will print the following :
+/*
+    *****
+    ****
+    ***
+    **
+    *
+
+ */
+
+void print_simple_pyramid()
+{
+    // insert code here
+}
+
+
+
+
+// You don't need to worry about the main function, you just need to complete the functions 
+int main()
+{
+    print_simple_pyramid();
+
+
+
+    return 0;
+}
