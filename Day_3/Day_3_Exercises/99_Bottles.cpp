54bd5ea4a23b50d5bfaa73711a369839200da21a
Philipp <psypdt@nottingham.ac.uk>
2019-06-26 12:32:41 +0200

    Update day3 bottle exercise

--- Day_3/Day_3_Exercises/99_Bottles.cpp
+++ Day_3/Day_3_Exercises/99_Bottles.cpp
@@ -10,7 +10,7 @@
 99 bottles of stuff on the wall, 99 bottles of stuff.
 Take one down pass it around 98 bottle of stuff on the wall.
 
-98 bottles of stuff on the wall, 99 bottles of stuff.
+98 bottles of stuff on the wall, 98 bottles of stuff.
 Take one down pass it around 97 bottle of stuff on the wall.
 ...
 ...
@@ -25,6 +25,12 @@ Can't take it down, can't pass it around because there are 0 bottles of stuff on
 // Your function should work with all number that are strictly greater than 0
 // Create your function here
 
+
+
+/*
+ Simply, I should be able to call your function from the main and give it an integer argument.
+ Your function should then count down from the input number until 0, followeing the pattern described above.
+ */
 
 
 int main()
